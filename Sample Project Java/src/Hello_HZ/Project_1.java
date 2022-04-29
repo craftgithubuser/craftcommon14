7501231a657bcad93e8c16d1855b12cf5fef4819
hanny2013 <104408772+hanny2013@users.noreply.github.com>
2022-04-29 01:15:21 -0400

    commit from eclipse by HannaZ

--- Sample Project Java/src/Hello_HZ/Project_1.java
+++ Sample Project Java/src/Hello_HZ/Project_1.java
@@ -5,9 +5,9 @@ public class Project_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-       System.out.println("Hello World1");
-       System.out.println("Hello World2");
-       System.out.println("Hello World3");
+       System.out.println("Hello World");
+       System.out.println("Hello Class");
+       System.out.println("Hello batch 26");
 	}
 
 }
