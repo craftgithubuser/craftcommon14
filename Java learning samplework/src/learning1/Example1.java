037eb1ce097ce52cec15e292a3b04a1da53a6e71
JerusalemT2 <elunewi@gmail.com>
2022-04-29 10:39:30 -0500

    commit from jerry

---
+++ Java learning samplework/src/learning1/Example1.java
@@ -0,0 +1,13 @@
+package learning1;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        System.out.print ("print paper");
+  
+	}
+}
+     
+
