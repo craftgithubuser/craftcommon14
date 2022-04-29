0f2acbefa5b9142c94302acac227528072382ecc
hanny2013 <104408772+hanny2013@users.noreply.github.com>
2022-04-29 00:55:47 -0400

    commit from eclipse

---
+++ Sample Project Java/src/Hello_HZ/Project_1.java
@@ -0,0 +1,12 @@
+package Hello_HZ;
+
+public class Project_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+       System.out.println("Hello World");
+       System.out.println("Hello World");
+	}
+
+}
