c58c63a4f158e812e61d2904c7ad74171d8ce1df
Mimi1973 <etse.haile2@gmail.com>
2022-04-29 20:46:56 -0400

    commit

---
+++ Second project/src/com/practice/Practice1.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
