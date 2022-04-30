9e9b66c64f335eca4299e784ba2e83f777968a87
musoabe <crafteknowledge.abeneg5@gamil.com>
2022-04-30 18:28:05 -0400

    A test class is added.

---
+++ Sample project 1/src/com/icraft1/Halloworld.java
@@ -0,0 +1,10 @@
+package com.icraft1;
+
+public class Halloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System .out.println("The is to say hallo world");
+	}
+
+}
