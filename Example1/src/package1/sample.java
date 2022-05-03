ac429512326eca5ce357cb056b4c7254c4030f50
heranb <hergezana@gmail.com>
2022-05-03 18:23:44 -0500

    commit from eclipse

---
+++ Example1/src/package1/sample.java
@@ -0,0 +1,20 @@
+package package1;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("print something");
+        System.out.println("print something");
+        System.out.println("print something");
+        System.out.println("print something");
+        System.out.println("print something");
+        System.out.println("print something");
+        System.out.println("print something");
+        System.out.println("print something");
+        System.out.println("print something");
+        System.out.println("print newthing");
+        System.out.println("print newthing");
+	}
+
+}
