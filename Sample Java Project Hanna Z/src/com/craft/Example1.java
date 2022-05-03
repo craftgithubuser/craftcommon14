2ebd2cab4064a6525550fef8300d61bf09cb66a6
hanny2013 <104408772+hanny2013@users.noreply.github.com>
2022-05-02 21:07:58 -0400

    Hanna Z commit from eclips

---
+++ Sample Java Project Hanna Z/src/com/craft/Example1.java
@@ -0,0 +1,10 @@
+package com.craft;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("First java project hanna Z");
+	}
+
+}
