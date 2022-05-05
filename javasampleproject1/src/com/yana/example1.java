1605e5b5bf8201c7a5559b226e716bcf4ad5a56b
netsiayni <netsicircus@gmail.com>
2022-05-05 00:18:05 -0400

    added file

---
+++ javasampleproject1/src/com/yana/example1.java
@@ -0,0 +1,22 @@
+package com.yana;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("print sameting");
+		System.out.println("print sameting");
+		System.out.println("print sameting");
+		System.out.println("print sameting");
+		System.out.println("print sameting");
+		System.out.println("print sameting");
+		System.out.println("print sameting");
+		System.out.println("print sameting");
+		System.out.println("print sameting");
+		System.out.println("print sameting");
+		
+		
+	}
+
+}
