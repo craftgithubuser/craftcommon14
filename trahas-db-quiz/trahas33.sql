93d3d8c6f6c9fc32377f070755a27c869bffa537
Nenu21 <gessesswt@gmail.com>
2022-05-12 00:25:59 -0400

    Create trahas33.sql

---
+++ trahas-db-quiz/trahas33.sql
@@ -0,0 +1,10 @@
+ use mydb101;
+insert into customers101(cust_ID, cust_firstname , cust_lastname, cust_age, cust_gender, 
+cust_country, cust_salary)
+ values (222, "Antony","Martial", 43,"m", "france", 10000000),
+(333," Nebiha", "Amir",32,  "F",  "uzebekistan", 36000000),
+(444, "soresa", "Hailu", 32,  "M", "unknown", 34000000),
+(555, "beka", "Haile", 28, "M",  "kazakhstan", 400000),
+(666, "Nick", "jones",45,  "M",  "Usa", "2000000"),
+(777, "poul", "pogba", 33, "M", "france",200000),
+(888,"Beti", "G", 65, "F", "ETHIOPIA", 700000);
