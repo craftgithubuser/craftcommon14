df40c6e90684d4da039234c297ce76837ad06ddb
ayu6910 <ayuny6910@gmail.com>
2022-05-11 23:25:29 -0400

    some comment

---
+++ Samsung/src/SamsungS21/Galaxy.java
@@ -0,0 +1,13 @@
+package SamsungS21;
+
+public class Galaxy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Ayzosh Geleme");
+		System.out.println("Ayzosh Geleme");
+		
+	}
+
+}
