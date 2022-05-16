d4d30c27f919115a1d99090c22f47c5f736cade7
Ariyos <ariyosbefikadu@gamil.com>
2022-05-16 04:40:24 -0400

    project from eclipse

---
+++ Addis-Ababa1/src/sheger/com/sheger_adugenet.java
@@ -0,0 +1,11 @@
+package sheger.com;
+
+public class sheger_adugenet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("Love Adugenet");
+	}
+
+}
