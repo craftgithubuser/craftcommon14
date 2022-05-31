a654339e1a5bfe177411536a43603cd9a537e54d
mahletgdt <mahletgdt@gmail.com>
2022-05-30 20:02:47 -0400

    array commit

---
+++ array/Challenge3.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Arrays;
+
+
+public class Challenge3 {
+
+public static void main(String[] args) {
+	    
+	    int[] array = new int[] {1,2,3,4,5};
+	    System.out.println("non reversed array: ");
+	    for(int i=0; i<array.length; i++) {
+	      System.out.print(array[i]+" ");
+	          
+	    }
+	    
+	    System.out.println();
+	    System.out.println("Reversed array: ");
+	    for(int i=array.length-1; i>=0; i--) {
+	      System.out.print(array[i] + " ");
+	    }
+	    
+	  }
+}
